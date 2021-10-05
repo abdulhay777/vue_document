a585ba5ced44c9e461d28186e87abe5a5c45e3fe
abdulhay <abdulhay.777.2020@gmail.com>
2021-10-05 17:55:30 +0500

    new lesson

---
+++ 29/vue-project-five/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app">
+        <h2>{{ msg }}</h2>
+        <h2>{{ msg | lowercase }}</h2>
+        <h2>{{ msg | uppercase }}</h2>
+
+        <input type="text" v-model="searchName">
+
+        <ul>
+            <li v-for="(list, i) in filterList" :key="i">{{ list }}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+            lists: ['Hello', 'Good'],
+            searchName: ''
+        }
+    },
+    computed: {
+        filterList() {
+            return this.lists.filter(list => {
+                return list.toLowerCase().indexOf(this.searchName) !== -1
+            })
+        }
+    },
+    filters: {
+        lowercase(value) {
+            return value.toLowerCase()
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ 29/vue-project-five/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.filter('uppercase', (value) => {
+    return value.toUpperCase()
+})
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
